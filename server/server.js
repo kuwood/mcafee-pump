07ec5f0f1a5be993a77d3806c2431532aa665540
Keith Underwood <keithunderwood510@gmail.com>
2018-01-25 16:08:50 -0800

    Add pre deploy script for dokku

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/ping', function (req, res) {
   return res.send('pong');
